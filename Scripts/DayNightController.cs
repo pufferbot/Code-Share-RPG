15703af09b33e94986f80165e780bd4a4e412101
Zoë <upsidedowncatfish4009@gmail.com>
2022-01-10 13:53:21 -0800

    1/10 school

---
+++ Scripts/DayNightController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+public class DayNightController : MonoBehaviour
+{
+    [Range(0, 24)]
+    public float timeOfDay;
+
+    public float orbitSpeed = 1.0f;
+    public Light sun;
+    public Light moon;
+    public Volume skyVolume;
+
+    public float starsBrightness = 7500f;
+    public AnimationCurve starsCurve;
+
+    private bool isNight;
+    private PhysicallyBasedSky sky;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skyVolume.profile.TryGet(out sky);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeOfDay += Time.deltaTime * orbitSpeed;
+        if (timeOfDay > 24)
+            timeOfDay = 0;
+
+        UpdateTime();
+    }
+
+    private void OnValidate()
+    {
+        skyVolume.profile.TryGet(out sky);
+        UpdateTime();
+    }
+
+    private void UpdateTime()
+    {
+        float alpha = timeOfDay / 24;
+
+        float sunRotation = Mathf.Lerp(-90, 270, alpha);
+        float moonRotation = sunRotation - 180;
+
+        sun.transform.rotation = Quaternion.Euler(sunRotation, -150.0f, 0);
+        moon.transform.rotation = Quaternion.Euler(moonRotation, -150.0f, 0);
+
+        sky.spaceEmissionMultiplier.value = starsCurve.Evaluate(alpha) * starsBrightness;
+
+        CheckNightDayTransition();
+    }
+
+    private void CheckNightDayTransition()
+    {
+        if (isNight)
+        {
+            if(moon.transform.rotation.eulerAngles.x > 180)
+                StartDay();
+        }
+        else
+        {
+            if (sun.transform.rotation.eulerAngles.x > 180)
+                StartNight();
+        }
+    }
+
+    private void StartDay()
+    {
+        isNight = false;
+        sun.shadows = LightShadows.Soft;
+        moon.shadows = LightShadows.None;
+    }
+
+    private void StartNight()
+    {
+        isNight = true;
+        sun.shadows = LightShadows.None;
+        moon.shadows = LightShadows.Soft;
+    }
+
+}
---
+++ Scripts/Quest System/TestScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : CustomNodeBehavior
+{
+
+    public override void RunScript()
+    {
+        print("Hello world! I am " + name);
+    }
+
+}
