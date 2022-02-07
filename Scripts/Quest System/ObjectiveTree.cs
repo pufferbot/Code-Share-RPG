5b38d0d60bac44187f2c7242fba94f1498d1b72b
Zoë <upsidedowncatfish4009@gmail.com>
2022-02-07 13:53:01 -0800

    2/7 school

--- Scripts/Quest System/ObjectiveTree.cs
+++ Scripts/Quest System/ObjectiveTree.cs
@@ -7,22 +7,25 @@ using UnityEngine;
 public class ObjectiveTree : ScriptableObject
 {
     public ObjectiveNode[] nodes;
+    public int[] startingNodes;
+
+    public void StartTree()
+    {
+        for(int i = 0; i < startingNodes.Length; i++)
+            nodes[startingNodes[i]].questObjective.Begin();
+    }
 
     public void NodeOutput(ObjectiveNode completedNode) //Run when a node's objective completes, and you want to run its outputs. Input the completed node
     {
         ObjectiveNode[] nodesToRun = completedNode.GetNextNodes(); //checks with the node to see which nodes come next
         foreach (ObjectiveNode node in nodesToRun)
-        {
             CheckInputs(node); //checks each to see if it should be started
-        }
     }
 
     public void CheckInputs(ObjectiveNode nodeToRun) //Checks to see if a node's input conditions have been met
     {
         if (nodeToRun.inputType == ObjectiveNode.InputType.Single || nodeToRun.inputType == ObjectiveNode.InputType.Merge) //Both single input and merge input nodes always run
-        {
             nodeToRun.questObjective.Begin();
-        }
         else if (nodeToRun.inputType == ObjectiveNode.InputType.Join) //Join input nodes will only run if all inputs are complete
         {
             bool allComplete = true;
--- Scripts/Quest System/Quest.cs
+++ Scripts/Quest System/Quest.cs
@@ -15,21 +15,30 @@ public class Quest : ScriptableObject
     public string questDescription;
 
     public int xpReward;
+    
     public QuestObjective[] questObjectives;
+    public ObjectiveTree objectiveTree;
 
-    [HideInInspector] public QuestState questState;
+    public QuestState questState;
+
+    public void CompleteObjective(int objective)
+    {
+        questObjectives[objective].Complete();
+        for (int i = 0; i < objectiveTree.nodes.Length; i++)
+            if(objectiveTree.nodes[i].questObjective == questObjectives[objective])
+                objectiveTree.NodeOutput(objectiveTree.nodes[i]);
+    }
 
     public void Begin()
     {
         if (questState == QuestState.Inactive)
         {
             questState = QuestState.Active;
+            objectiveTree.StartTree();
             Debug.Log("Started quest " + questName + ".");
         }
         else
-        {
             Debug.LogError("Quest " + questName + " has already been started.");
-        }
     }
 
     public void Complete()
@@ -41,13 +50,9 @@ public class Quest : ScriptableObject
             Debug.Log("Completed quest " + questName + ".");
         }
         else if (questState == QuestState.Inactive)
-        {
             Debug.LogError("Quest " + questName + " has not been started yet.");
-        }
         else
-        {
             Debug.LogError("Quest " + questName + " has already been finished.");
-        }
     }
 
     public void Fail()
@@ -58,13 +63,9 @@ public class Quest : ScriptableObject
             Debug.Log("Failed quest " + questName + ".");
         }
         else if (questState == QuestState.Inactive)
-        {
             Debug.LogError("Quest " + questName + " has not been started yet.");
-        }
         else
-        {
             Debug.LogError("Quest " + questName + " has already been finished.");
-        }
     }
 
 }
